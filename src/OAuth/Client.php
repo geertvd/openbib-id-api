a4b4f8192bad7c4e0c0e5cda9dd6c0dc444cbb89
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2017-01-18 11:39:07 +0100

    Fix indentation.

--- src/OAuth/Client.php
+++ src/OAuth/Client.php
@@ -11,15 +11,16 @@ class Client extends OAuthClient
     /**
      * {@inheritdoc}
      */
-    public function __construct($oauthOptions, $uri = null, $config = null) {
-      if ($oauthOptions !== null) {
-          if ($oauthOptions instanceof \Traversable) {
-              $oauthOptions = ArrayUtils::iteratorToArray($oauthOptions);
-          }
-          if (!isset($oauthOptions['token'])) {
-            $oauthOptions['token'] = new Access();
-          }
-      }
-      parent::__construct($oauthOptions, $uri, $config);
+    public function __construct($oauthOptions, $uri = null, $config = null)
+    {
+        if ($oauthOptions !== null) {
+            if ($oauthOptions instanceof \Traversable) {
+                $oauthOptions = ArrayUtils::iteratorToArray($oauthOptions);
+            }
+            if (!isset($oauthOptions['token'])) {
+              $oauthOptions['token'] = new Access();
+            }
+        }
+        parent::__construct($oauthOptions, $uri, $config);
     }
 }
